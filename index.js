0a44d221228f92dee2fd9ddb71549361fc573be2
Antokoko <antonellakokoko@gmail.com>
2023-01-09 14:56:46 -0500

    update files

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 //cambios
 const port = process.env.PORT || 3000;
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public/"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
