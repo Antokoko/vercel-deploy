04cbaff8c05738f43b4d432908919e8cc92f87e5
Antokoko <antonellakokoko@gmail.com>
2023-01-09 11:57:15 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+//cambios
+const port = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName,
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us11.api.mailchimp.com/3.0/lists/c519e0d986";
+
+    const options = {
+        method: "POST",
+        auth: "grecia3:663734234f2a6bab8c1e0a6ff810b3f0-us11"
+    }
+
+    const request1 = https.request(url, options, function(response) {
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        });
+    });
+
+    request1.write(jsonData);
+    request1.end();
+});
+
+app.post("/failure", function(req, res){
+    res.redirect("/")
+});
+
+app.post("/success", function(req, res){
+    res.redirect("/")
+})
+
+app.listen(port, function(){
+    console.log("Server is running on port 3000");
+});
